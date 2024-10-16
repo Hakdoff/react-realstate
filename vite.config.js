04f61c12d6dd61dd875d9cabc2505b0af0749f26
ErikaFuerte <erika@bpoc.co.jp>
2024-10-16 15:32:11 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/react-realstate/', // Add this line
+  base: '/react-realstate/',  // This should match your repository name
 })
