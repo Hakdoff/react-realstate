2d02dd70af9d24d789dda142f0a49f608ea00e4a
ErikaFuerte <erika@bpoc.co.jp>
2024-10-16 13:17:15 +0800

    npm run build

---
+++ src/index.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App.jsx"; // Adjust the path if needed
+
+ReactDOM.render(<App />, document.getElementById("root"));
